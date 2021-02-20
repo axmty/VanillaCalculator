974afbd3b77123339bcbe6022bd48b519069454e
axmty <alexandre.metgy@gmail.com>
2021-02-20 13:48:23 +0100

    Encapsulate some code and handle "-" operator display.

--- app.js
+++ app.js
@@ -1,50 +1,96 @@
 "use strict";
 
-const resultLine = document.getElementById("calc-result");
-
-let expression = (function() {
+const expression = (function() {
     let expr = ["0"];
 
     function last() {
         return expr[expr.length - 1];
     }
     
-    function append(value) {
-        expr[expr.length] = value;
+    function append(symbol) {
+        expr[expr.length] = symbol;
     }
     
     function length() {
         return expr.length;
     }
 
+    function lastIndex() {
+        return expr.length - 1;
+    }
+
+    function addNumber(number) {
+        const l = last();
+        if (l === "0") {
+            expr[lastIndex()] = number.toString();
+        } else if (isNaN(+last())) {
+            append(number.toString());
+        } else {
+            expr[lastIndex()] += number;
+        }
+    }
+
+    function addDot() {
+        const l = last();
+        if (isNumber(l) && l.indexOf(".") < 0) {
+            expr[lastIndex()] += ".";
+        } else if (isNaN(l)) {
+            append(".");
+        }
+    }
+
+    function addOperator(operator) {
+        const l = last();
+        if (operator === "-") {
+            if ((length() === 1 && l === "0") || l === "+") {
+                expr[lastIndex()] = "-";
+            } else if (l !== "-") {
+                append("-");
+            }
+        } else if (isNumber(last())) {
+
+        }
+    }
+
+    function allClear() {
+        expr = ["0"];
+    }
+
+    function isOperator(symbol) {
+        return "+-*/".indexOf(symbol) >= 0;
+    }
+
+    function isNumber(value) {
+        return Number.isFinite(+value);
+    }
+
+    function isNaN(value) {
+        return Number.isNaN(value);
+    }
+
     return {
-        addNumber(number) {
-            if (length() === 1 && expr[0].toString() === "0") {
-                expr[0] = number.toString();
-            } else {
-                if (Number.isNaN(+last())) {
-                    append(number);
-                } else {
-                    expr[expr.length - 1] += number;
-                }
+        addSymbol(symbol) {
+            if (typeof symbol === "number") {
+                addNumber(symbol);
+            } else if (symbol === ".") {
+                addDot();
+            } else if (symbol === "AC") {
+                allClear();
+            } else if (isOperator(symbol)) {
+                addOperator(symbol);
             }
-        },
-        addDot() {
-            const l = last();
-            if (Number.isFinite(+l) && l.indexOf(".") < 0) {
-                expr[expr.length - 1] += ".";
-            } else if (Number.isNaN(l)) {
-                append("0.");
-            }
-        },
-        allClear() {
-            expr = ["0"];
         },
         toString() {
             let text = "";
-            expr.forEach((v, i) => {
-                if (Number.isFinite(+v)) {
-                    text += v;
+            expr.forEach((s, i) => {
+                if (isNumber(s)) {
+                    text += s;
+                } else if (isOperator(s)) {
+                    if (i > 0 && isNumber(expr[i - 1])) {
+                        text += ` ${s} `;
+                    } else {
+                        text += s;
+                    }
                 }
             });
             return text;
@@ -52,18 +98,15 @@ let expression = (function() {
     }
 })();
 
-function onClickButton(symbol) {
-    if (typeof symbol === "number") {
-        expression.addNumber(symbol);
-    } else if (symbol === ".") {
-        expression.addDot();
-    } else if (symbol === "AC") {
-        expression.allClear();
+const printer = (function(element) {
+    return {
+        printExpression(expression) {
+            element.textContent = expression.toString();
+        }
     }
+})(document.getElementById("calc-result"));
 
-    printExpression();
-}
-
-function printExpression() {
-    resultLine.textContent = expression.toString();
+function onClickButton(symbol) {
+    expression.addSymbol(symbol);
+    printer.printExpression(expression);
 }
